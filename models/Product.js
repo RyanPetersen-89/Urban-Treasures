ea9398f148fc4b3c1115f9da0e01d53209dc5462
Ryan Petersen <rp26898@gmail.com>
2024-07-03 19:21:43 -0700

    Completed the category/Product.js model page for defining product data rules.

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,47 @@
-// import important parts of sequelize library
+// requires sequalize libray
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+// Imports the database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // Here we defined the fields of data types, and set rules for the product data
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        //utilizes the category model to utilize the key: id
+        model: 'category',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
